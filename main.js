12c236febdfaecac09d19a6af2b96e87ee3adb30
Goran Jakovljevic <goranefbl@gmail.com>
2016-04-22 15:09:37 +0200

    quick fix

--- main.js
+++ main.js
@@ -1,50 +1,50 @@
 (function() {
 
-	'use strict';
+    'use strict';
 
-	window.GENSPlugin = function () {
+    window.GENSPlugin = function() {
 
-		// Global element references, to reference piece of plugin anywhere
-	    this.someElement = null;
+        // Global element references, to reference piece of plugin anywhere
+        this.someElement = null;
 
-		// Default Options
-		var defaults = {
-	        firstOption  : true,
-	        secondOption : 1
-		}
+        // Default Options
+        var defaults = {
+            firstOption: true,
+            secondOption: 1
+        }
 
-		// Create options by extending defaults with the passed in arguments
-	    if (arguments[0] && typeof arguments[0] === "object") {
-	      this.options = extendDefaults(defaults, arguments[0]);
-	    }
+        // Create options by extending defaults with the passed in arguments
+        if (arguments[0] && typeof arguments[0] === "object") {
+            this.options = extendDefaults(defaults, arguments[0]);
+        }
 
-	}
+    }
 
-	// Public Method
-	GENSPlugin.prototype.init = function() { 
-		
-	}
+    // Public Method
+    GENSPlugin.prototype.init = function() {
 
-	// Private Methods
-	function somePrivateMethod() {
+    }
 
-	}
+    // Private Methods
+    function somePrivateMethod() {
 
-	// Extend default Options
-	function extendDefaults(defaults, properties) {
-		var property;
-		for (property in properties) {
-			if (properties.hasOwnProperty(property)) {
-				defaults[property] = properties[property];
-			}
-		}
-		return defaults;
-	}
+    }
+
+    // Extend default Options
+    function extendDefaults(defaults, properties) {
+        var property;
+        for (property in properties) {
+            if (properties.hasOwnProperty(property)) {
+                defaults[property] = properties[property];
+            }
+        }
+        return defaults;
+    }
 
 }());
 
 var GENSplugin = new GENSPlugin({
-	firstOption  : false,
-	secondOption : 5 
+    firstOption: false,
+    secondOption: 5
 });
 GENSplugin.init();
